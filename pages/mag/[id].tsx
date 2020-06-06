f1654f3a2d1e559f9f6fb47282642d463d127fb0
Sho Tokuda <haze.it.acc@gmail.com>
2020-06-06 21:45:54 +0900

    バージョンを変えてみる

--- pages/mag/[id].tsx
+++ pages/mag/[id].tsx
@@ -28,7 +28,7 @@ export default (props: Props) => {
   return (
     <>
       <p>{props.id}: Build At {props.buildAt}</p>
-      <p>version: 1</p>
+      <p>version: 2</p>
       <Loading />
     </>
   );
